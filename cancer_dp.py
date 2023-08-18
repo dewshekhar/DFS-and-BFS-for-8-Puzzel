c209e1e0152e404baa6fe7aad9dd68aa375ddf92
dewshekhar <dewshekhar@gmail.com>
2023-08-18 12:54:04 +0530

    Update cancer_dp.py

--- cancer_dp.py
+++ cancer_dp.py
@@ -1,77 +1,90 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd             # To work with data in tables
+import numpy as np              # For math calculations
+import matplotlib.pyplot as plt # To create plots
 
-# Load the dataset from the provided GitHub link
+# Load the dataset from the provided link
 url = "https://raw.githubusercontent.com/dewshekhar/DFS-and-BFS-for-8-Puzzel/main/cancer.csv"
 data = pd.read_csv(url)
 
-# Drop non-numeric columns and constant columns
-data_numeric = data.select_dtypes(include=[np.number])
-data_numeric = data_numeric.loc[:, ~data_numeric.columns.duplicated()]
-data_numeric = data_numeric.dropna(axis=1, how='all')  # Drop columns with all missing values
+# Drop columns that are not numbers and those with all missing values
+numeric_data = data.select_dtypes(include=[np.number])
+numeric_data = numeric_data.loc[:, ~numeric_data.columns.duplicated()]
+numeric_data = numeric_data.dropna(axis=1, how='all')
 
-# Handle missing values using mean imputation
-data_imputed = data_numeric.fillna(data_numeric.mean())
+# Fill missing values with average values
+imputed_data = numeric_data.fillna(numeric_data.mean())
 
 # Standardize the data
-data_scaled = (data_imputed - data_imputed.mean()) / data_imputed.std()
+scaled_data = (imputed_data - imputed_data.mean()) / imputed_data.std()
 
-# Number of clusters
+# Number of clusters we want
 num_clusters = 2
 
-# Manual K-Means clustering
-def manual_k_means(data, k, num_iterations=100):
+# Custom K-Means clustering
+def custom_k_means(data, k, num_iterations=100):
     np.random.seed(0)
-    centroids = data.sample(n=k, random_state=0).values
+    # Initialize random cluster centers
+    centers = data.sample(n=k, random_state=0).values
     for _ in range(num_iterations):
-        distances = np.linalg.norm(data.values[:, np.newaxis] - centroids, axis=2)
-        labels = np.argmin(distances, axis=1)
-        new_centroids = np.array([data[labels == i].mean().values for i in range(k)])
-        if np.all(new_centroids == centroids):
+        # Calculate distances from data points to centers
+        distances = np.linalg.norm(data.values[:, np.newaxis] - centers, axis=2)
+        # Assign each data point to the nearest center
+        cluster_labels = np.argmin(distances, axis=1)
+        # Calculate new centers by finding the average of points in each cluster
+        new_centers = np.array([data[cluster_labels == i].mean().values for i in range(k)])
+        # Check for convergence
+        if np.all(new_centers == centers):
             break
-        centroids = new_centroids
-    return labels
+        centers = new_centers
+    return cluster_labels
 
-kmeans_labels = manual_k_means(data_scaled, num_clusters)
+kmeans_labels = custom_k_means(scaled_data, num_clusters)
 
-# Manual K-Medoids clustering
-def manual_k_medoids(data, k, num_iterations=100):
+# Custom K-Medoids clustering
+def custom_k_medoids(data, k, num_iterations=100):
     np.random.seed(0)
+    # Initialize random medoids indices
     medoids_indices = np.random.choice(data.shape[0], k, replace=False)
     medoids = data.values[medoids_indices]
     for _ in range(num_iterations):
+        # Calculate distances between data points and medoids
         distances = np.linalg.norm(data.values[:, np.newaxis] - medoids, axis=2)
-        labels = np.argmin(distances, axis=1)
-        new_medoids = np.array([data.values[labels == i].mean(axis=0) for i in range(k)])
+        # Assign data points to the nearest medoid
+        cluster_labels = np.argmin(distances, axis=1)
+        # Calculate new medoids as the data point with the smallest total distance to other points in its cluster
+        new_medoids = np.array([data.values[cluster_labels == i].mean(axis=0) for i in range(k)])
+        # Check for convergence
         if np.all(new_medoids == medoids):
             break
         medoids = new_medoids
-    return labels
+    return cluster_labels
 
-kmedoids_labels = manual_k_medoids(data_scaled, num_clusters)
+kmedoids_labels = custom_k_medoids(scaled_data, num_clusters)
 
-# Visualize the clusters and count the number of points in each cluster
+# Plot the clusters and count points in each cluster
 plt.figure(figsize=(12, 6))
 
-# Select "radius_mean" and "texture_mean" for plotting
-X_plot = data_imputed[['radius_mean', 'texture_mean']].values
+# Choose two attributes for plotting
+plot_data = imputed_data[['radius_mean', 'texture_mean']].values
 
 # Plot K-Means clusters
 plt.subplot(1, 2, 1)
-plt.scatter(X_plot[:, 0], X_plot[:, 1], c=kmeans_labels, cmap='viridis', alpha=0.5)
+plt.scatter(plot_data[:, 0], plot_data[:, 1], c=kmeans_labels, cmap='viridis', alpha=0.5)
 plt.xlabel('radius_mean')
 plt.ylabel('texture_mean')
-plt.title('Manual K-Means Clustering')
+plt.title('Custom K-Means Clustering')
 plt.colorbar(label='Cluster')
 
 # Plot K-Medoids clusters
 plt.subplot(1, 2, 2)
-plt.scatter(X_plot[:, 0], X_plot[:, 1], c=kmedoids_labels, cmap='viridis', alpha=0.5)
+plt.scatter(plot_data[:, 0], plot_data[:, 1], c=kmedoids_labels, cmap='viridis', alpha=0.5)
 plt.xlabel('radius_mean')
 plt.ylabel('texture_mean')
-plt.title('Manual K-Medoids Clustering')
+plt.title('Custom K-Medoids Clustering')
 plt.colorbar(label='Cluster')
+
+# Display author information
+plt.figtext(0.5, 0.01, 'Author: DEWASHISH PRAMANIK\nAdmission No: IITP001202', ha='center', fontsize=10)
 
 plt.tight_layout()
 plt.show()
